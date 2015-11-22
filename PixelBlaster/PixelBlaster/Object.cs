9c1c35d2d72c80130b402b1b71c6dd2a01477d5d
jollyfitguy <bensonprice@gmail.com>
2015-11-22 13:02:52 -0700

    Commit PixelBlaster to repo

---
+++ PixelBlaster/PixelBlaster/Object.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+
+namespace PixelBlaster
+{
+    class Object
+    {
+        public CircleShape shape { get; set; }
+
+        public Object()
+        {
+            shape.FillColor = Color.Green;
+            shape.Radius = 50F;
+        }
+
+
+
+    }
+}
---
+++ PixelBlaster/PixelBlaster/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SFML;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace PixelBlaster
+{
+    class Program
+    {
+        //Instantiate the game and start it
+        static void Main(string[] args)
+        {
+            Game app = new Game();
+            app.StartGame();
+        }
+    }
+}
